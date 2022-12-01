f0b0be4a2fc7463edb3819dedbbe2a36ef401e8b
David Hdz <davidhernandeze@gmail.com>
2022-12-01 12:26:48 -0600

    add openrelay turn servers

--- src/App.vue
+++ src/App.vue
@@ -124,7 +124,22 @@ onBeforeMount(async () => {
 const peerConnection = new RTCPeerConnection({
   iceServers: [
     {
-      urls: import.meta.env.VITE_STUN_SERVER
+      urls: 'stun:openrelay.metered.ca:80'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
     }
   ]
 })
